0a01029a3f127e61f31052057e60dcc79a4e61e5
Alex Miles <howdy@alexmilesdesign.com>
2013-07-21 12:09:55 +0200

    Fix detection if statement. Clean up indentation.
    Album redirect now works

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 function rdify() {
 
-	// The page url
+  // The page url
   var url = window.location.href;
-  
+
   // Split url into an array
   var splitUrl = url.split('/');
 
@@ -23,31 +23,31 @@ function rdify() {
 
   // Spotify API 
   var spotifySearch = 'http://ws.spotify.com/lookup/1/.json?uri=spotify:' + pageType + ':' + trackID;
-  
+
   // Rdio search URL
   var rdiosearch = 'http://www.rdio.com/search/';
 
   // Retrieve JSON data from Spotify
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", spotifySearch, true);
-	xhr.onreadystatechange = function() {
-	  if (xhr.readyState == 4) {
-	    // JSON.parse does not evaluate the attacker's scripts.
-	    var resp = JSON.parse(xhr.responseText);
-	    var type = (resp.info.type);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", spotifySearch, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts.
+      var resp = JSON.parse(xhr.responseText);
+      var type = (resp.info.type);
 
-	    // Detect if the page is an album or track
-	 		if (type = 'track') {
-	    	var title = resp.track.name;
-	    	console.log(title);
-	    	window.location.href = rdiosearch + encodeURIComponent(title);
-	    } else if (type = 'album') {
-	    	var name = resp.album.name;
-	    	console.log(name);
-	    	window.location.href = rdiosearch + encodeURIComponent(name);
-	    }
-	  }
-	}
+      // Detect if the page is an album or track
+      if (type == 'track') {
+        var title = resp.track.name;
+        console.log(title);
+        window.location.href = rdiosearch + encodeURIComponent(title);
+      } else if (type == 'album') {
+        var name = resp.album.name;
+        console.log(name);
+        window.location.href = rdiosearch + encodeURIComponent(name);
+      }
+    }
+  };
 	xhr.send();
 }
 
